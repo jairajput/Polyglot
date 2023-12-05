9621dd287560a553010db1257087365138cf084d
Jai <jai07rajput@gmail.com>
2023-12-05 18:43:52 +0530

    ch 02 Hello World

---
+++ C++/ch 02 Hello World/main.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  ch 02 Hello World
+//
+//  Created by Jai  on 05/12/23.
+//
+
+
+//Hello World Program
+//The "Hello World" program is a simple yet traditional introduction to any programming language. It helps beginners understand the basic structure and syntax of a program.
+
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
+/* #include <iostream>: This line includes the Input/Output stream library in the program, which is necessary for using cout for output.
+ int main(): The main function is the entry point. Every C++ program starts executing from here.
+ std::cout: Used for output. Here, it prints "Hello, World!" to the console.
+ return 0;: Indicates that the program has ended successfully.
+ 
+ */
